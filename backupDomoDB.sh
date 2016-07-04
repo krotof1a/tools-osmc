c66d28d8d4651bd697e6e20b7b5facdc2591eb07
krotof1a <christophe.safra@gmail.com>
2016-07-04 12:06:08 +0200

    Some updates

--- backupDomoDB.sh
+++ backupDomoDB.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O /media/sda1/backups/domoticz.db "http://localhost:5665/backupdatabase.php" > /dev/zero 2>&1
+wget -O /media/sda1/backups/domoticz.db "http://192.168.1.17:5665/backupdatabase.php" > /dev/zero 2>&1
 chmod 666 /media/sda1/backups/domoticz.db
 
